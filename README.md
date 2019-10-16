7565eabdbeb15db361ffedb2dc183b5da09d09a6
Paul Rudin <paul@rudin.co.uk>
2019-10-16 09:20:27 +0100

    get travis to build and push docker image to hub

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Provides SSO for k8s ingresses using the nginx ingress controller utilising an
 existing OIDC provider.
   
 This software is a work in progress, DO NOT attempt to use in production.
+
--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,6 @@ ROOT="$(dirname "$(dirname "$(readlink -fm "${BASH_SOURCE[0]}")")")"
 
 CMD="cd ${ROOT} && make push"
 
+echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 eval ${CMD}
 
